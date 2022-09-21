2a207077260c30af844493ee809f1100a46c266d
T. J. Silva <thiago.silva@imd.ufrn.br>
2022-09-21 10:16:44 -0300

    🏗️ feat: add architecture module

---
+++ rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/RpgturnosDomainApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.pds.rpgturnosdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgturnosDomainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RpgturnosDomainApplication.class, args);
+  }
+
+}
---
+++ rpgturnos-domain/src/test/java/br/ufrn/pds/rpgturnosdomain/RpgturnosDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.pds.rpgturnosdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgturnosDomainApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
