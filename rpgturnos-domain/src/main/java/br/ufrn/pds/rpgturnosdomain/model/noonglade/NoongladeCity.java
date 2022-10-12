2bed175b52ddeb0b11fea169a62e4eac8930f06c
T. J. Silva <thiago.silva@imd.ufrn.br>
2022-10-12 18:46:16 -0300

    #8 💬 refactor: update text and literals

--- rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/model/noonglade/NoongladeCity.java
+++ rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/model/noonglade/NoongladeCity.java
@@ -8,11 +8,12 @@ public class NoongladeCity {
 	private static final Scanner scan = new Scanner(System.in);
 
 	public static void beginning() {
-		System.out.println("\nSeus olhos se abrem e você rola para fora de sua cama de lençol. A luz do sol está brilhando em seu quarto.\nSua mente ainda está nebulosa, então você deve se lembrar qual é o seu nome.");
-		System.out.print("\nDigite o seu nome: ");
+		System.out.println("\n\"Seus olhos se abrem e você rola para fora de sua cama de lençol. A luz do sol está brilhando em seu quarto.\nSua mente ainda está nebulosa, então você deve se lembrar qual é o seu nome…\"");
+		System.out.print("\n- Diga-me seu nome forasteiro!");
+		System.out.print("\n- Eu me chamo… ");
 		String name = scan.nextLine();
 		Character.setName(name);
-		System.out.println("\nAh, isso mesmo! Seu nome é " + Character.getName() + ".");
-		System.out.println("\nVocê boceja e depois sai. Está um lindo dia em sua cidade natal de Noonglade.\nAs pessoas já estão lá fora se misturando e os pássaros estão cantando.");
+		System.out.println("- Ah, isso mesmo! Seu nome é " + Character.getName() + ".");
+		System.out.println("\n\"Você boceja e depois sai. Está um lindo dia em sua cidade natal de Noonglade.\nAs pessoas já estão lá fora se misturando e os pássaros estão cantando.\"");
 	}
 }
