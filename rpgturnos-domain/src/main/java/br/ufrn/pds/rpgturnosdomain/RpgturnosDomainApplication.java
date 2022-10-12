86c00b93de01837769831739cf5f5f4d1efe799f
T. J. Silva <thiago.silva@imd.ufrn.br>
2022-10-12 17:24:48 -0300

    #8 ✨ feat: add first city

--- rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/RpgturnosDomainApplication.java
+++ rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/RpgturnosDomainApplication.java
@@ -1,13 +1,15 @@
 package br.ufrn.pds.rpgturnosdomain;
 
+import br.ufrn.pds.rpgturnosdomain.model.noonglade.NoongladeCity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RpgturnosDomainApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(RpgturnosDomainApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(RpgturnosDomainApplication.class, args);
+		NoongladeCity.beginning();
+	}
 
 }
