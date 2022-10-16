40acc95b5033de74b41683554872b5f0bca88683
T. J. Silva <thiago.silva@imd.ufrn.br>
2022-10-16 18:35:09 -0300

    #8 🎨 refactor: improve format of the code

--- rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/model/noonglade/NoongladeCity.java
+++ rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/model/noonglade/NoongladeCity.java
@@ -11,10 +11,14 @@ public class NoongladeCity {
 		System.out.println("\n\"Seus olhos se abrem e você rola para fora de sua cama de lençol. A luz do sol está brilhando em seu quarto.\nSua mente ainda está nebulosa, então você deve se lembrar qual é o seu nome…\"");
 		System.out.print("\n- Diga-me, como é o seu nome?");
 		System.out.print("\n- Eu me chamo… ");
+
 		String name = scan.nextLine();
 		Character.setName(name);
+
 		System.out.println("- Ah, isso mesmo! Seu nome é " + Character.getName() + ".");
+
 		Character.setRole();
-		System.out.println("\n\"Você boceja e depois sai. Está um lindo dia em sua cidade natal de Noonglade.\nAs pessoas já estão lá fora se misturando e os pássaros estão cantando.\"");
+
+		System.out.println("\n\"Você boceja e depois sai. Está um lindo dia em sua cidade natal, Noonglade.\nAs pessoas já estão lá fora se misturando e os pássaros estão cantando.\"");
 	}
 }
