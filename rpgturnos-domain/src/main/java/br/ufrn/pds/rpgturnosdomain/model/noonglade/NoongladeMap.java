4ca4313b4486c00c31570f87f3f8a218ac35587f
T. J. Silva <thiago.silva@imd.ufrn.br>
2022-10-17 10:24:41 -0300

    #3 🎨 refactor: print map

--- rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/model/noonglade/NoongladeMap.java
+++ rpgturnos-domain/src/main/java/br/ufrn/pds/rpgturnosdomain/model/noonglade/NoongladeMap.java
@@ -1,10 +1,14 @@
 package br.ufrn.pds.rpgturnosdomain.model.noonglade;
 
+import br.ufrn.pds.rpgturnosdomain.model.assets.Character;
+
 import java.util.Scanner;
 
 public class NoongladeMap {
 	private static final Scanner scan = new Scanner(System.in);
 	private static String move;
+	private static final int xAxisPosition = 4;
+	private static final int yAxisPosition = 3;
 	private static final int top = 7;
 	private static final String[] map = {
 		"^^^^^^^^^^        ≈≈≈≈≈≈≈  ^^^^",
@@ -30,6 +34,23 @@ public class NoongladeMap {
 	public static void printMap() {
 		System.out.println("===============================");
 
+		for (int aKey = 0; aKey < top; ++aKey) {
+			System.out.println(map[aKey]);
+		}
+
+		System.out.println(map[top].substring(0, xAxisPosition));
+		System.out.println(Character.printSymbol());
+		System.out.println(map[top].substring(xAxisPosition + 1, map[top].length()));
+
+		for (int dKey = 0; dKey < top + yAxisPosition; ++dKey) {
+			System.out.println(map[dKey + 1]);
+		}
+
 		System.out.println("===============================");
+		System.out.println(
+			"\nLife: " + Character.getLife() + "/" + Character.getMaxLife() +
+			"\t\tX: " + xAxisPosition +
+			"\tY: " + yAxisPosition
+		);
 	}
 }
